e1b29f14ef9f6077f9808c48a39b531555dc11df
Vitalijs Smirnovs <g00317774@nuig.ie>
2019-03-14 13:09:42 +0000

    Mar14_2

---
+++ problem1.py
@@ -0,0 +1,20 @@
+# This is a work by
+# Vitalijs Smirnovs
+# Stydent ID #
+
+# Problem 1 :
+# Solution
+
+# Ask user for input
+# Use int() as integer is needed
+num = int(input("Please enter a positive integer: " ))
+# create a list in range from 1 to user
+# input number "num" +1. 
+# range function does not include the last number specified
+i=list(range(1,num+1))
+
+# function sum adds up all the numbers in the list 
+Sum = sum(i)
+# print the result on the screen
+print(Sum)  
+     
---
+++ tuesday.py
@@ -0,0 +1,11 @@
+# Ian McLoughlin, 2018-02-01
+# Is it Tuesday?
+
+import datetime
+
+if datetime.datetime.today().weekday() == 6:
+  print("Yay! It is Sunday.")
+else:
+  print("Unfortunately it is not Sunday.")
+print("Hello world")
+
