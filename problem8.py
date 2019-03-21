c8b54e1d4691f396a774f71c97562e5ed1236fd6
Vitalijs Smirnovs <g00317774@nuig.ie>
2019-03-21 00:05:41 +0000

    Mar21-1

---
+++ problem8.py
@@ -0,0 +1,12 @@
+import datetime
+
+t = datetime.datetime.now()
+
+print(t.strftime("%A, %B %dth %Y at %I:%M%p,"))
+
+#t = datetime.datetime(2019, 1, 10, 1, 15)
+
+#print(t.strftime("%A, %B %dth %Y "" at "" %H:%M%p")) 
+#x = datetime.datetime(2019, 1, 10)
+
+#print(x.strftime("%A, %B %dth %Y %H:%M%p"))
